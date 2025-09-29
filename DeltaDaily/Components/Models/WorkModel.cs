e24d994db309d95a702ed08fd73b77251d0f9637
Marcelo Scalvi <scalvi.marcelo@gmail.com>
2025-09-29 16:03:08 -0300

    Planner

--- DeltaDaily/Components/Models/WorkModel.cs
+++ DeltaDaily/Components/Models/WorkModel.cs
@@ -4,8 +4,8 @@ namespace DeltaDaily.Components.Models
 {
     public class WorkModel
         {
-        public ProjectModel Plano { get; set; }          
-        public string? Duracao { get; set; }
+        public string? ProjetoId { get; set; }
+        public TimeOnly? Duracao { get; set; }
         public WorkData.WorkType? Tipo { get; set; }
         public WorkData.Focus? Foco { get; set; }
         public WorkData.WorkMethod? Metodo { get; set; }
