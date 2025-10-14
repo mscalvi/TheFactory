b5d744d6e2dc1e7aee7e956d7da4adfca3ee3ec3
mscalvi <scalvi.marcelo@gmail.com>
2025-10-14 11:31:50 -0300

    Models

---
+++ FurmaIdle/Models/StageModel.cs
@@ -0,0 +1,20 @@
+﻿using FurmaIdle.Helpers;
+
+namespace FurmaIdle.Models
+{
+    public class StageModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Icon { get; set; }
+        public List<string> Images { get; set; }
+        public string Description { get; set; }
+        public string UnlockId { get; set; }
+        public int Level { get; set; }
+        public string PricingId { get; set; }
+        public EnumHelper.State State { get; set; }
+        public EnumHelper.Persistence Persistence { get; set; }
+        public int PartySizeStart { get; set; }
+        public int PartySizeMax { get; set; }
+    }
+}
---
+++ FurmaIdle/Models/TechModel.cs
@@ -0,0 +1,17 @@
+﻿using FurmaIdle.Helpers;
+
+namespace FurmaIdle.Models
+{
+    public class TechModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Icon { get; set; }
+        public string Description { get; set; }
+        public string UnlockId { get; set; }
+        public int Level { get; set; }
+        public string PricingId { get; set; }
+        public EnumHelper.State State { get; set; }
+        public EnumHelper.Persistence Persistence { get; set; }
+    }
+}
